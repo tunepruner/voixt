b0e5ee0396411c5a029373cb05af705145bcf1b5
philcarlson <writephilcarlson@gmail.com>
2023-02-20 19:38:09 -0500

    Implements theme and connects wherever possible

--- app/src/main/java/com/tunepruner/voixt/ui/theme/Theme.kt
+++ app/src/main/java/com/tunepruner/voixt/ui/theme/Theme.kt
@@ -10,31 +10,27 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = MediumLightBackground,
+    secondary = MediumDarkBackground,
+    tertiary = ExtraDarkBackground,
+    background = ExtraDarkBackground,
+    surface = MediumLightBackground,
+    onPrimary = LightText,
+    onSecondary = MediumLightBackground,
+    onTertiary = MediumDarkBackground,
+    onBackground = MediumDarkBackground,
+    onSurface = LightText,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    /** TODO we have no light theme designed. Will there even be one?*/
 )
 
 @Composable
@@ -44,14 +40,15 @@ fun VoixtTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = DarkColorScheme
+//        when {
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
+//        darkTheme -> DarkColorScheme
+//        else -> LightColorScheme
+//    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
