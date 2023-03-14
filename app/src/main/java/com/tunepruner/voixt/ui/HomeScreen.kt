0bf9bb21094c42faed9f5c4dd56d561935b3541e
philcarlson <writephilcarlson@gmail.com>
2023-03-14 12:07:48 -0400

    Completes theming work on HomeScreen.kt

--- app/src/main/java/com/tunepruner/voixt/ui/HomeScreen.kt
+++ app/src/main/java/com/tunepruner/voixt/ui/HomeScreen.kt
@@ -1,18 +1,34 @@
 package com.tunepruner.voixt.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tunepruner.voixt.ui.theme.VoixtTheme
 
-@Preview
+val paddingBetweenRowsAndButtons = 2.dp
+val buttonShape = RoundedCornerShape(5.dp)
+
+val gradientColorForButtons = Brush.verticalGradient(
+    listOf(
+        Color(0x536F6F6F),
+        Color(0x80404040),
+    )
+)
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    VoixtTheme {
+        HomeScreen()
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,7 +51,7 @@ fun TopBar() {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp), color = Color.Gray
+            .height(200.dp)
     ) {
 
     }
@@ -43,7 +59,7 @@ fun TopBar() {
 
 @Composable
 fun HomeScreenNavigationButtonsArea() {
-    Column {
+    Column(modifier = Modifier.padding(paddingBetweenRowsAndButtons)) {
         HomeScreenSingleButtonRow(modifier = Modifier.weight(1f))
         HomeScreenSingleButtonRow(modifier = Modifier.weight(1f))
         HomeScreenTwoButtonRow(modifier = Modifier.weight(1f))
@@ -71,10 +87,19 @@ fun RowScope.HomeScreenNavigationButton(modifier: Modifier = Modifier) {
         modifier = modifier
             .weight(1f)
             .fillMaxSize()
-            .padding(5.dp), onClick = {
+            .padding(paddingBetweenRowsAndButtons)
+            .background(shape = buttonShape, brush = gradientColorForButtons),
+        onClick = {
             println("clicked")
-        }, shape = RoundedCornerShape(5.dp)
+        },
+        shape = buttonShape,
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
     ) {
-        Text("My button")
+        Text(
+            text = "My button",
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize()
+        )
     }
 }
